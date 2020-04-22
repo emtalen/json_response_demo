ad0449f2f206d3ba6a804615020221deb79d3a84
emtalen <59577366+emtalen@users.noreply.github.com>
2020-04-22 14:42:06 +0200

    unit testing the comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_db_column :body}
+  it { is_expected.to validate_presence_of :body}
+  it { is_expected.to belong_to :user }
+  it { is_expected.to belong_to :article }
+
 end
